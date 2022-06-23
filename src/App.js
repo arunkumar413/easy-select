40708423f287e9820aa624c22b6d76cabfa9df4e
Arun Kumar <arunkumar413@gmail.com>
2022-06-23 07:34:02 +0000

    edited css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import './style.css';
-import { EasySelect } from './EasySelect';
+import React, { useState, useEffect } from "react";
+import "./style.css";
+import { EasySelect } from "./EasySelect";
 
 export default function App() {
-  const [selected, setSelected] = useState({ label: '', value: '' });
+  const [selected, setSelected] = useState({ label: "", value: "" });
 
   const selectOptions = [
-    { label: 'January', value: 0 },
-    { label: 'February', value: 1 },
-    { label: 'March', value: 2 },
-    { label: 'April', value: 3 },
-    { label: 'May', value: 4 },
-    { label: 'June', value: 5 },
-    { label: 'July', value: 6 },
-    { label: 'March', value: 7 },
+    { label: "January", value: 0 },
+    { label: "February", value: 1 },
+    { label: "March", value: 2 },
+    { label: "April", value: 3 },
+    { label: "May", value: 4 },
+    { label: "June", value: 5 },
+    { label: "July", value: 6 },
+    { label: "March", value: 7 },
   ];
 
   function handleSelect(evt, item) {
@@ -23,12 +23,13 @@ export default function App() {
 
   return (
     <div className="App">
-      {' '}
+      {" "}
       <EasySelect
+        multiSelect={false}
         value={selected}
         options={selectOptions}
         onSelect={handleSelect}
-      />{' '}
+      />{" "}
     </div>
   );
 }
--- src/EasySelect.js
+++ src/EasySelect.js
@@ -1,12 +1,16 @@
-import React, { useState, useEffect, useRef } from 'react';
-import './style.css';
-import ClickAwayListener from 'react-click-away-listener';
+import React, { useState, useEffect, useRef } from "react";
+import "./style.css";
+import ClickAwayListener from "react-click-away-listener";
 
 export function EasySelect(props) {
-  const [isDropDownOpen, setDropDown] = useState('none');
+  const [isDropDownOpen, setDropDown] = useState("none");
   const ref = useRef(null);
 
   function handleSelect(evt, item) {
+    props.onSelect(evt, item);
+  }
+
+  function addSelection(evt, item) {
     props.onSelect(evt, item);
   }
 
@@ -24,17 +28,22 @@ export function EasySelect(props) {
 
   function displayDropDown(evt) {
     // evt.stopPropagation();
-    setDropDown('block');
+    setDropDown("block");
   }
 
   function hideDropDown() {
-    setDropDown('none');
+    setDropDown("none");
   }
 
   return (
     <ClickAwayListener onClickAway={hideDropDown}>
       <div onClick={displayDropDown} className="easy-select-container">
-        <div className="selected-container">
+        <div
+          className="selected-container"
+          style={
+            isDropDownOpen === "block" ? { border: "solid 0.5px blue" } : {}
+          }
+        >
           <span className="selected-item"> {props.value.label} </span>
           <span className="material-symbols-outlined arrow">expand_more</span>
         </div>
